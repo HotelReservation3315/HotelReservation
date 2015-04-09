53343ce598edb4e2cd1a5c9d3965297e01323822
melsn_000 <melsn_000@204.158.167.236>
2015-04-09 08:24:42 -0500

    GUI update

--- HotelReservation/src/hotelReservation/MainFrame.java
+++ HotelReservation/src/hotelReservation/MainFrame.java
@@ -2,6 +2,7 @@ package hotelReservation;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -30,40 +31,58 @@ public class MainFrame extends JFrame implements ActionListener{
 	public static final int XCOORD = 100;
 	public static final int YCOORD = 80;
 	
+	JLabel checkInTitle;
 	JLabel checkInLabel;
 	JLabel checkOutLabel;
 	JLabel typeOfRoomLabel;
 	JLabel specificRoomLabel;
 	JLabel locationOfRoomLabel;
+	JLabel numberOfPeopleLabel;
 	
 	JComboBox <String> typeOfRoom;
 	JComboBox <String> specificRoom;
 	JComboBox <String> locationOfRoom;
+	JComboBox <Integer> numberOfPeople;
 
 	final JTextField checkInTextField;
 	final JTextField checkOutTextField;
+	
+	JButton CheckAvailabilityButton;
+	JButton CheckInOption;
+	JButton CheckOutOption;
 	
 	
 	
 	public MainFrame(){
 			
-		 checkInLabel = new JLabel("Check-in:"); 
+		checkInTitle = new JLabel("CHECK-IN:");
+		checkInTitle.setFont(new Font("Courier", Font.BOLD, 17));
+		checkInTitle.setBounds(10, 0, 100, 50);
+		checkInTitle.setVisible(false);
+		
+		 checkInLabel = new JLabel("Check-in Date:"); 
 		 checkInTextField = new JTextField(10);
 		 
-		 	checkInLabel.setBounds(10, 10, 100, 50);
-		 	checkInTextField.setBounds(80,25,100,20);
+		 	checkInLabel.setBounds(10, 30, 100, 50);
+		 	checkInLabel.setVisible(false);
+		 	checkInTextField.setBounds(100,45,100,20);
+		 	checkInTextField.setVisible(false);
 	 
-		 checkOutLabel = new JLabel("Check-out:"); 
+		 checkOutLabel = new JLabel("Check-out Date:"); 
 		 checkOutTextField = new JTextField(10);
 		 
-		 	checkOutLabel.setBounds(10, 50, 100, 50);
-		 	checkOutTextField.setBounds(80,65,100,20);
+		 	checkOutLabel.setBounds(10, 60, 100, 50);
+		 	checkOutLabel.setVisible(false);
+		 	checkOutTextField.setBounds(110,75,100,20);
+		 	checkOutTextField.setVisible(false);
 		 
 		 typeOfRoomLabel = new JLabel("Type of Room:");
 		 typeOfRoom = new JComboBox <String> ();
 		 			 
 		 	typeOfRoomLabel.setBounds(10, 90, 130, 50);
+		 	typeOfRoomLabel.setVisible(false);
 		 	typeOfRoom.setBounds(100,105,80,20);
+		 	typeOfRoom.setVisible(false);
 			 
 		 		typeOfRoom.addItem("Luxury");
 		 		typeOfRoom.addItem("Cottage");
@@ -73,7 +92,9 @@ public class MainFrame extends JFrame implements ActionListener{
 		 		specificRoomLabel = new JLabel("Characteristics:");
 		 		specificRoom = new JComboBox <String> ();
 		 		specificRoomLabel.setBounds(10, 120, 130, 50);
+		 		specificRoomLabel.setVisible(false);
 	 			specificRoom.setBounds(110,135,120,20);
+	 			specificRoom.setVisible(false);
 		 
 	 			typeOfRoom.addItemListener(new ItemListener() {
 
@@ -97,22 +118,26 @@ public class MainFrame extends JFrame implements ActionListener{
 		            }
 		        });
 
-	 			locationOfRoomLabel = new JLabel("Location of Room:");
-	 			locationOfRoom = new JComboBox <String> ();
+	 	locationOfRoomLabel = new JLabel("Location of Room:");
+	 	locationOfRoom = new JComboBox <String> ();
 	 						 
-	 			locationOfRoomLabel.setBounds(10, 150, 130, 50);
-	 			locationOfRoom.setBounds(120,165,75,20);
+	 		locationOfRoomLabel.setBounds(10, 150, 130, 50);
+	 		locationOfRoomLabel.setVisible(false);
+	 		locationOfRoom.setBounds(120,165,75,20);
+	 		locationOfRoom.setVisible(false);
 	 				 
 	 			locationOfRoom.addItem("Patio");
 	 			locationOfRoom.addItem("Forest");
 	 			//locationOfRoom.addItem("Any");
 	 			locationOfRoom.setSelectedItem(null); // Doesn't select any option by default in the ComboBox.
 		 	 				 	
-		 /*JLabel numberOfPeopleLabel = new JLabel("Number of People:");
-		 JComboBox <Integer> numberOfPeople = new JComboBox <Integer> ();
+		numberOfPeopleLabel = new JLabel("Number of People:");
+		numberOfPeople = new JComboBox <Integer> ();
 		 
-		 	numberOfPeopleLabel.setBounds(10, 90, 130, 50);
-		 	numberOfPeople.setBounds(125,105,45,20);
+		 	numberOfPeopleLabel.setBounds(10, 180, 130, 50);
+		 	numberOfPeopleLabel.setVisible(false);
+		 	numberOfPeople.setBounds(120,195,45,20);
+		 	numberOfPeople.setVisible(false);
 		 
 		 		numberOfPeople.addItem(1);
 		 		numberOfPeople.addItem(2);
@@ -121,26 +146,27 @@ public class MainFrame extends JFrame implements ActionListener{
 		 		numberOfPeople.addItem(5);
 		 		numberOfPeople.addItem(6);
 		 		numberOfPeople.addItem(7);
+		 		numberOfPeople.setSelectedItem(null); // Doesn't select any option by default in the ComboBox.
 		 		
-		 JLabel numberOfRoomsLabel = new JLabel("Number of Rooms:");
-		 JComboBox <Integer> numberOfRooms = new JComboBox <Integer> ();
+		 CheckAvailabilityButton = new JButton("Check Availability");
+		 CheckAvailabilityButton.setBounds(20, 240, 150, 25);
+		 CheckAvailabilityButton.setVisible(false);
+		 CheckAvailabilityButton.addActionListener(this);
 		 
-		 	numberOfRoomsLabel.setBounds(10, 120, 130, 50);
-		 	numberOfRooms.setBounds(125,135,45,20);
+		 CheckInOption = new JButton("Check-In");
+		 CheckInOption.setBounds(140, 110, 150, 25);
+		 CheckInOption.addActionListener(this);
 		 
-		 		numberOfRooms.addItem(1);
-		 		numberOfRooms.addItem(2);
-		 		numberOfRooms.addItem(3);
-		 		numberOfRooms.addItem(4);
-		 		numberOfRooms.addItem(5);
-		 		numberOfRooms.addItem(6);
-		 		numberOfRooms.addItem(7);*/
+		 CheckOutOption = new JButton("Check-Out");
+		 CheckOutOption.setBounds(140, 160, 150, 25);
+		 CheckOutOption.addActionListener(this);
 		 
 		 JPanel p = new JPanel();
 		 p.setLayout(null);
 		 	
 		 final JFrame frame = new JFrame(); 
 		 	
+		 		p.add(checkInTitle);
 		 		p.add(checkInLabel); 
 		 		p.add(checkInTextField); 
 		 		p.add(checkOutLabel); 
@@ -151,11 +177,12 @@ public class MainFrame extends JFrame implements ActionListener{
 		 		p.add(specificRoom);
 		 		p.add(locationOfRoomLabel);
 		 		p.add(locationOfRoom);
-		 		//p.add(numberOfPeople);
-		 		//p.add(numberOfPeopleLabel);
-		 		//p.add(numberOfRooms);
-		 		//p.add(numberOfRoomsLabel);
-		 	
+		 		p.add(numberOfPeople);
+		 		p.add(numberOfPeopleLabel);
+		 		p.add(CheckAvailabilityButton);
+		 		p.add(CheckOutOption);
+		 		p.add(CheckInOption);
+		 				 	
 		 
 		 frame.add(p); 
 		 frame.setSize(WIDTH, HEIGHT);
@@ -178,8 +205,7 @@ public class MainFrame extends JFrame implements ActionListener{
 		 			checkOutTextField.setText(selectedDate); 
 		 		} 
 		 	});
-		 			
-				
+		 				
 		paintComponents(getGraphics());
 		
 	}
@@ -194,10 +220,72 @@ public static void main(String[] args){
 
 
 public void actionPerformed(ActionEvent event) {
-
-	/*if(event.getSource() == button1){
+	
+	if(event.getSource() == CheckInOption){
+		checkInTitle.setVisible(true);
+		checkInLabel.setVisible(true);
+		checkOutLabel.setVisible(true);
+		typeOfRoomLabel.setVisible(true);
+		specificRoomLabel.setVisible(true);
+		locationOfRoomLabel.setVisible(true);
+		numberOfPeopleLabel.setVisible(true);
 		
-		String name = NameField.getText();
+		typeOfRoom.setVisible(true);
+		specificRoom.setVisible(true);
+		locationOfRoom.setVisible(true);
+		numberOfPeople.setVisible(true);
+
+		checkInTextField.setVisible(true);
+		checkOutTextField.setVisible(true);
+		
+		CheckAvailabilityButton.setVisible(true);
+		
+		CheckInOption.setVisible(false);
+		CheckOutOption.setVisible(false);
+	}
+	
+	if(event.getSource() == CheckOutOption){
+		
+	}
+
+	if(event.getSource() == CheckAvailabilityButton){
+		
+		if(typeOfRoom.getSelectedItem() != null && specificRoom.getSelectedItem() != null && 
+			locationOfRoom.getSelectedItem() != null && numberOfPeople.getSelectedItem() != null){ // Checks if the fields are completed by the user.
+			
+		String toR = typeOfRoom.getSelectedItem().toString();
+		String chaR = specificRoom.getSelectedItem().toString();
+		String loR = locationOfRoom.getSelectedItem().toString();
+		
+			
+			if(toR == "Luxury"){
+				if(loR == "Patio"){
+					if(chaR == "1 Queen bed"){
+						// ACCESS DATABASE LOOKING FOR LUXURY, PATIO AND 1 QUEEN BED. RETRIEVE NUMBER OF ROOMS AVAILABLE.
+					}else if(chaR == "2 Queen beds"){
+						
+					}else if(chaR == "Two Room"){
+						
+					}else if(chaR == "Three Room"){
+						
+					}else if(chaR == "Bridal"){
+						
+					}
+				}else if(loR == "Forest"){
+					if(chaR == "Two Room"){
+						// ACCESS DATABASE LOOKING FOR LUXURY, FOREST AND TWO ROOM. RETRIEVE NUMBER OF ROOMS AVAILABLE.
+					}else if(chaR == "Three Room"){
+						
+					}else if(chaR == "Four Room"){
+						
+					}
+				}
+			}
+		}else{
+			JOptionPane.showMessageDialog(null, "Please select an option from all fields.");
+		}
+		
+		/*String name = NameField.getText();
 	
 		try {
 			Class.forName( "sun.jdbc.odbc.JdbcOdbcDriver" );
@@ -218,9 +306,9 @@ public void actionPerformed(ActionEvent event) {
 	    catch ( ClassNotFoundException classNotFound ) { // detect problems loading database driver
 	        JOptionPane.showMessageDialog( null, classNotFound.getMessage(), "Driver Not Found", JOptionPane.ERROR_MESSAGE );
 	        System.exit( 1 );
-	    }
+	    }*/
 		
-	}*/
+	}
 	
 }
 
